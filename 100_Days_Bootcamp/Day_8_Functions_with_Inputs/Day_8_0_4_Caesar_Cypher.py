e0da1bbc290ba3f52873bb6c5b41ad08eb93cec5
fmemeteau <131686141+fmemeteau@users.noreply.github.com>
2023-09-28 16:52:07 +0200

    Update Day_8_0_4_Caesar_Cypher.py

--- 100_Days_Bootcamp/Day_8_Functions_with_Inputs/Day_8_0_4_Caesar_Cypher.py
+++ 100_Days_Bootcamp/Day_8_Functions_with_Inputs/Day_8_0_4_Caesar_Cypher.py
@@ -9,15 +9,16 @@ def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == 'decode':
             shift_amount *= -1
-    for letter in start_text:
+    for char in start_text:
         # 3 - What happens if the user enters a number / symbol / space ?
         # Can you fix the code to keep the number/symbol/space when the text is encoded/decoded ?
-        if letter not in alphabet:
-            end_text += letter
-        else:
-            position = alphabet.index(letter)
+        if char in alphabet:
+            position = alphabet.index(char)
             new_position = position + shift_amount
             end_text += alphabet[new_position]
+        else:
+            end_text += char
+            
     print(f"The {direction}d text is {end_text}.")
 
 # 1 - Import and print the logo from Art.py when the program start.
@@ -28,14 +29,20 @@ print(logo)
 # e.g. Type 'yes' if you want to go again, otherwise type 'no'.
 # If they type 'yes', then ask for the direction/text/shift again and call the caesar() function again.
 # Hint : create a new function that calls itself if they type 'yes'.
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
 # 2 - What if the user enters a shift that is greater than the number of letters in the alphabet ?
 # Hint : Think about how you can use the modulus.
 
-shift = shift % 26
+    shift = shift % 26
 
-caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
+    caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
+
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == 'no':
+         should_continue = False
+         print("Goodbye")
