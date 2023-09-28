2727984f390574c8bcdeaf2d4e43b34ffb4ad2c5
fmemeteau <131686141+fmemeteau@users.noreply.github.com>
2023-09-28 21:05:12 +0200

    Create Day_9

---
+++ 100_Days_Bootcamp/Day_9_Dictionaries_Nested-Lists/Day_9_0_Secret_Auction.py
@@ -0,0 +1,34 @@
+# A dictionary, be it in Python or from real life, can be seen as a table, where the left part (a word in a dictionary)
+# is a 'key' and the right part (the definition) is the 'value'
+
+# The syntax to create a dictionary in Python is 'dictionary = {Key: Value}'
+# Copying the example from the course would be :
+# {"Bug": "An error in a program that prevents the program from running as expected."}
+
+# Each Key / Value pair is separated by a comma :
+# {"Bug": "Something",
+# "Function": "A piece of code that you can easily call over and over again." 
+# }
+
+# Retrieving an item from a dictionary :
+# print(dictionary["Bug"])
+
+# Adding a new item :
+# dictionary["Loop"] = "The action of doing something over and over again."
+
+# Create an empty dictionary :
+# dictionary  = {}
+
+# Wipe an entire dictionary :
+# existing_dictionary = {}
+
+# Edit an item in a dictionary, simply write a new value :
+# dictionary["Bug"] = "A moth in your computer"
+
+# Loop through a dictionary :
+# For key in dictionary:
+#   print(key)
+# would simply display the name of the keys.
+# To display the values as well, do :
+# For key in dictionary:
+#   print(dictionary[key]) 
---
+++ 100_Days_Bootcamp/Day_9_Dictionaries_Nested-Lists/Day_9_1_Grading_Program.py
@@ -0,0 +1,21 @@
+# You have access to a database of student_scores in the format of a dictionary. The keys are the names of the students
+# and the values are their exam scores. Write a program that converts thei scores to grades.
+# By the end of the program, you should have a new dictionary called student_grades containing the names as keys and their grades as values.
+
+# This is the scoring criteria:
+#   91 - 100 : "Outstanding"
+#   81 - 90 : "Exceeds Expectations"
+#   71 - 80: "Acceptable"
+#   70 or lower: "Fail"
+
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62
+}
+
+
+
+print(student_grades)
